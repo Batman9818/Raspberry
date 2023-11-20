04a9abb0a526542dd64724fe3f9100fb035f2add
Yami1106 <130268606+Yami1106@users.noreply.github.com>
2023-11-20 19:32:02 +0530

    Update pir_sensor.py

--- pir_sensor.py
+++ pir_sensor.py
@@ -1,18 +1,16 @@
 import RPi.GPIO as GPIO
-import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(5,GPIO.OUT)
-try:
-  while True:
-         i=GPIO.input(12)
-         if i==1:
-             print("MOTION DETECTED",i)
-             GPIO.output(5,1)
-             time.sleep(0.1)
-         else:
-             print("no MOTION DETECTED",i)
-             GPIO.output(5,0)
-except KeyboardInterrupt:
-         GPIO.cleanup()
+PIR_input = 29				
+LED = 32				
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)		
+GPIO.setup(PIR_input, GPIO.IN)	
+GPIO.setup(LED, GPIO.OUT)
+GPIO.output(LED, GPIO.LOW)
+
+while True:
+#when motion detected turn on LED
+    if(GPIO.input(PIR_input)):
+        GPIO.output(LED, GPIO.HIGH)
+    else:
+        GPIO.output(LED, GPIO.LOW)
