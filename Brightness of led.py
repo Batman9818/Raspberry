a9c29ff23ea7123541bb7cf4d05caf9569b2a9fe
Batman9818 <150887683+Batman9818@users.noreply.github.com>
2023-11-20 12:04:59 +0530

    Update Brightness of led.py

--- Brightness of led.py
+++ Brightness of led.py
@@ -8,11 +8,13 @@ pwm=gpio.PWM(pin,500)
 pwm.start(100)
 try:
     while True:
+        gpio.output(pin,True)
         time.sleep(1)
         duty=int(input("Enter Duty Cycle:"))
         if duty<0 and duty>100:
             print("CYCLE VALUE SHOULD BE AROUND 1 TO 100")
             continue
+        gpio.output(pin,False)
         time.sleep(1)
         pwm.ChangeDutyCycle(duty)
 except KeyboardInterrupt:
